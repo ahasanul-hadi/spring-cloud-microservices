4cbb031bf0bfcb6de1e72f41e823ec6ce85d25f8
unknown <ahasanul.hadi@gmail.com>
2023-04-11 12:13:40 +0600

    Initial commit

---
+++ src/main/java/com/demoproject/springcloudmicroservices/SpringCloudMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.demoproject.springcloudmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudMicroservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demoproject/springcloudmicroservices/SpringCloudMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoproject.springcloudmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
